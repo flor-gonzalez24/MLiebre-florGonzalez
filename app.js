a7de8450fefbd4781240fb833f838a40be77cc6f
flor-gonzalez24 <florgonzalez4374@gmail.com>
2024-06-29 15:22:37 -0300

    1° paso: Subiendo el proyecto

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require ('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+
+
+app.post('/submit-form', (req, res) => {
+    res.redirect('/');
+  });
+
+
+app.listen(3030, () => 
+    console.log("Servidor corriendo"))
+    
+    
